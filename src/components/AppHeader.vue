8f7e68cae20b35f679831551ba22cd2566a7b949
Akniet.Kh <akniet.khakimov@mail.ru>
2020-07-22 18:43:37 +0700

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="header">
+    <h1 class="logo">Star Wars Persons</h1>
+    <nav class="navigation">
+      <router-link to='/'>Главная</router-link>
+      <router-link to='/favorite'>Избранные</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    // width: 100%;
+    min-height: 10rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 4rem;
+    border-bottom: 2px solid #ffffff;
+  }
+
+  .logo {
+    font-size: 4.2rem;
+    font-weight: 600;
+  }
+
+  .navigation {
+    max-width: 30rem;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center; 
+
+    a {
+      box-sizing: border-box;
+      height: 3.2rem;
+      font-size: 2.4rem;
+      line-height: 4.2rem;
+      text-decoration: none;
+      color: #ffffff;
+      cursor: pointer;
+
+      &:hover {
+        border-bottom: 1px solid #ffffff;
+      }
+      &:active {
+        opacity: 0.8;
+        border-bottom: 1px solid #ffffff;
+      }
+    }
+  }
+</style>
