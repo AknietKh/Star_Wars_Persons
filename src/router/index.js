6070953ad7c9dd29f7473817dfb112aa187ab999
Akniet.Kh <akniet.khakimov@mail.ru>
2020-07-22 18:48:32 +0700

    add home page and add route for page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      layout: 'main'
+    }
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <card-container></card-container>
+    <app-pagination></app-pagination>
+  </div>
+</template>
+
+<script>
+import CardContainer from '@/components/CardContainer.vue'
+import Pagination from '@/components/Pagination.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CardContainer,
+    'app-pagination': Pagination
+  },
+  created: function() {
+    this.$store.dispatch('GET_CARDS')
+  }
+}
+</script>
