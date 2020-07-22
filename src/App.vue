944170024d88b5f8fc8da2069da436d444b6de9c
Akniet.Kh <akniet.khakimov@mail.ru>
2020-07-22 18:42:42 +0700

    add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <div id="app">
-    
+    <component :is='layout'>
+      <router-view></router-view>
+    </component>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import MainLayout from '@/layouts/MainLayout.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout
+  },
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'main') + '-layout';
+    }
+  }
 }
+</script>
+
+<style lang="scss">
+@import "@/assets/style/reset.scss";
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    background-color: #102027;
+  }
+
+  html {
+    font-size: 10px;    
+  }
 
 </style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-layout">
+    <app-header></app-header>
+    <main class="main-content">
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script>
+import AppHeader from '../components/AppHeader.vue'
+
+export default {
+  name: 'MainLayout',
+  components: {
+    AppHeader,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .main-layout {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .main-content {
+    box-sizing: border-box;
+    flex-grow: 1;
+    width: 100%;
+    padding: 4rem 10rem;
+  }
+</style>
