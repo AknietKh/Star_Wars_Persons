168ed3d1e39ac0fa2720dcf146b68f1a0d322be1
Akniet.Kh <akniet.khakimov@mail.ru>
2020-07-22 21:15:18 +0700

    add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/views/Home.vue'
+import NotFoundPage from '@/views/NotFoundPage.vue'
 
 Vue.use(VueRouter)
 
@@ -12,6 +13,10 @@ const routes = [
     meta: {
       layout: 'main'
     }
+  },
+  {
+    path: '*',
+    component: NotFoundPage
   }
 ]
 
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="not-found">
+    <h1 class="info-text">Страница не найдена</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage'
+}
+</script>
+
+<style lang="scss" scoped>
+  .not-found {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .info-text {
+    font-weight: 300;
+    font-size: 3.6rem;
+    line-height: 4.2rem;
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+</style>
